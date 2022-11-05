e576aabb8d061bfecf97afe33e3e918b7a42c20e
EastWon0103 <dongwon000103@gmail.com>
2022-11-06 03:28:49 +0900

    add: deploy-commands.js file

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const {REST, Routes} = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('node:fs');
+
+dotenv.config();
+// command 등록
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            {body: commands},
+        );
+    
+        console.log(`Successfully reloaded ${data.length}`);
+    } catch (error) {
+        console.log(error);
+    }
+})();
